8a0a23c6cbb3fcbfc1bc4da3dc66a377a2ea68c3
TerenceLiu98 <terenceliu1012@gmail.com>
2022-08-28 03:34:42 +0000

    fix Endpoint

--- wgtools/add.py
+++ wgtools/add.py
@@ -17,7 +17,7 @@ def network(filename:str="wg0"):
             f.write(network_info)
         return 0
 
-def node(filename:str="wg0", nodename:str="node1"):
+def node(filename:str="wg0", nodename:str="node1", pubaddr:str="1.1.1.1"):
     config = configparser.ConfigParser()
     config.optionxform = str
     config.read("{}.conf".format(filename))
@@ -25,6 +25,7 @@ def node(filename:str="wg0", nodename:str="node1"):
     node.Name = nodename
     node.Address.append("".join(config["Network"]["addr4Pool"][:-4] + str(random.randint(1, 255)) + "/24"))
     node.ListenPort = random.randint(32768, 60999)
+    node.Endpoint = pubaddr + ":" + str(node.ListenPort) 
     node.PrivateKey, node.PublicKey = wgc_genkey()
     node.AllowedIPs.append("".join(node.Address[0][:-3] + "/32"))
     node_info = "\n[{}]\n".format(node.Name) + to_toml(node).replace('"', "")
@@ -39,4 +40,4 @@ if __name__ == "__main__":
         if sys.argv[1] == "network":
             network(filename=sys.argv[2])
         else:
-            node(filename=sys.argv[1], nodename=sys.argv[2])
+            node(filename=sys.argv[1], nodename=sys.argv[2], pubaddr=sys.argv[3])
